d795fb721e5178b14b11bd23ca4b9e51b7611a18
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2025-03-25 14:58:20 +0100

    definer registernavn i DTO

--- src/main/kotlin/no/nav/arbeid/registeroppslag/bemanningsforetak/BemanningsforetakDTO.kt
+++ src/main/kotlin/no/nav/arbeid/registeroppslag/bemanningsforetak/BemanningsforetakDTO.kt
@@ -1,11 +1,10 @@
 package no.nav.arbeid.registeroppslag.bemanningsforetak
 
-import com.fasterxml.jackson.databind.JsonNode
 import no.nav.arbeid.registeroppslag.Organisasjonsnummer
 import no.nav.arbeid.registeroppslag.Registerstatus
+import no.nav.arbeid.registeroppslag.RegisterstatusDTO
 
 data class BemanningsforetakDTO(
-    val registernavn: String = "bemanningsforetaksregisteret",
     val organisasjonsnummer: Organisasjonsnummer,
     val overordnetEnhet: String?,
     val navn: String,
@@ -15,19 +14,7 @@ data class BemanningsforetakDTO(
     val underenheter: List<BemanningsforetakDTO>?,
 ) {
     companion object {
-        fun fraJson(jsonNode: JsonNode): BemanningsforetakDTO {
-            if (jsonNode.isEmpty) return ikkeRegistrert()
-
-            return BemanningsforetakDTO(
-                organisasjonsnummer = Organisasjonsnummer(jsonNode["organisasjonsnummer"].asText()),
-                overordnetEnhet = jsonNode["overordnetEnhet"]?.asText(),
-                navn = jsonNode["navn"].asText(),
-                registerstatus = Registerstatus.entries[jsonNode["registerstatus"].asInt()],
-                registerstatusTekst = jsonNode["registerstatusTekst"].asText(),
-                godkjenningsstatus = jsonNode["godkjenningsstatus"].asText(),
-                underenheter = jsonNode["underenheter"]?.map { fraJson(it) } ?: emptyList(),
-            )
-        }
+        val registernavn = "bemanningsforetaksregisteret"
 
         fun ikkeRegistrert(): BemanningsforetakDTO {
             return BemanningsforetakDTO(
@@ -40,6 +27,14 @@ data class BemanningsforetakDTO(
                 underenheter = emptyList(),
             )
         }
+    }
+
+    fun tilRegisterstatus(): RegisterstatusDTO {
+        return RegisterstatusDTO(
+            registernavn = registernavn,
+            statusTekst = godkjenningsstatus,
+            status = registerstatus,
+        )
     }
 }
 
