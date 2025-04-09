3ec220841421c9cd36357e5666fbd4fa82b5e311
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2025-04-09 13:07:43 +0200

    legg til mulighet for å spesifisere headere i forespørselen

--- src/main/kotlin/no/nav/arbeid/registeroppslag/RegisterParser.kt
+++ src/main/kotlin/no/nav/arbeid/registeroppslag/RegisterParser.kt
@@ -14,14 +14,19 @@ interface RegisterParser {
 
     fun parseRegister(register: ByteArray): List<Any>
 
-    fun lastNedRegisterData(registerNavn: String, registerUrl: URI, httpClient: HttpClient): ByteArray {
-        val request = HttpRequest.newBuilder(registerUrl)
+    fun lastNedRegisterData(registerNavn: String, registerUrl: URI, httpClient: HttpClient, headers: Map<String, String> = emptyMap()): ByteArray {
+        val requestBuilder = HttpRequest.newBuilder(registerUrl)
             .header("Accept", "application/json")
             .GET()
-            .build()
+
+        if (headers.isNotEmpty())    {
+            headers.forEach { (key, value) ->
+                requestBuilder.header(key, value)
+            }
+        }
 
         log.info("Laster ned $registerNavn fra $registerUrl")
-        val response = httpClient.send(request, BodyHandlers.ofByteArray())
+        val response = httpClient.send(requestBuilder.build(), BodyHandlers.ofByteArray())
         return when (response.statusCode()) {
             200 -> response.body()
             else -> throw RuntimeException("Feil ved forespørsel mot $registerNavn, ${response.statusCode()}: ${response.body().decodeToString()}")
