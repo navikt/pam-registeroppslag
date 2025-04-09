5c01602fa7a54e2a0b323e556ee632b814f10ecb
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2025-04-09 13:08:27 +0200

    opprett metrikk for nedlastet bilpleieregister

--- src/main/kotlin/no/nav/arbeid/registeroppslag/metrikker/Metrikker.kt
+++ src/main/kotlin/no/nav/arbeid/registeroppslag/metrikker/Metrikker.kt
@@ -5,7 +5,9 @@ import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 class Metrikker(meterRegistry: PrometheusMeterRegistry) {
     private val bemanningsforetakNedlastetCounter = meterRegistry.counter("pam_registeroppslag_nedlasting_bemanningsforetak")
     private val renholdsregisterNedlastetCounter = meterRegistry.counter("pam_registeroppslag_nedlasting_renholdsregister")
+    private val bilpleieregisterNedlastetCounter = meterRegistry.counter("pam_registeroppslag_nedlasting_bilpleieregister")
 
     fun lastetNedOgLagretBemmaningsforetak() = bemanningsforetakNedlastetCounter.increment()
     fun lastetNedOgLagretRenholdsregister() = renholdsregisterNedlastetCounter.increment()
+    fun lastetNedOgLagretBilpleieregister() = bilpleieregisterNedlastetCounter.increment()
 }
